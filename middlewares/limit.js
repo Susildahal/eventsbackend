3c239d5f49a38d38992fb23c58cc134b84513512
sushil dahal <susildahal234@gmail.com>
2025-12-15 13:59:14 +0545

    add a limit

--- middlewares/limit.js
+++ middlewares/limit.js
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit';
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, 
-  limit: 2, 
+  limit: 2000, 
   standardHeaders: 'draft-8',
   legacyHeaders: false,
 
