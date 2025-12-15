b6d5a0b51cc2da3307260953afe6d85b04efc2fe
sushil dahal <susildahal234@gmail.com>
2025-12-15 13:55:45 +0545

    test the limit

--- middlewares/limit.js
+++ middlewares/limit.js
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit';
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, 
-  limit: 1000, 
+  limit: 2, 
   standardHeaders: 'draft-8',
   legacyHeaders: false,
 
